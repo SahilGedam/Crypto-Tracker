25962d94d230e392af9622889cb32cf651ca604a
Sahil Gedam <gedamsahil1997@gmail.com>
2024-08-27 22:46:53 +0530

    Services Added

--- crypto-tracker/src/app/app.component.ts
+++ crypto-tracker/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'crypto-tracker';
+  selectedCountry = 'INR';
+  constructor(){}
+  sendCurrency($event: any){
+    console.log(event);
+    
+  }
 }
--- crypto-tracker/src/app/app.module.ts
+++ crypto-tracker/src/app/app.module.ts
@@ -3,11 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { CoinListComponent } from './components/coin-list/coin-list.component';
+import { CoinDetailComponent } from './components/coin-detail/coin-detail.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  declarations: [AppComponent, CoinListComponent, CoinDetailComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatSelectModule,FormsModule
+  ],
+  providers: [provideAnimations()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
